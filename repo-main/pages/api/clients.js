0970e6575d9827b408c98525c7bda52c1aaabb83
stefan91bg <95471012+stefan91bg@users.noreply.github.com>
2025-10-16 18:52:57 +0200

    Update clients.js

--- repo-main/pages/api/clients.js
+++ repo-main/pages/api/clients.js
@@ -23,8 +23,11 @@ export default async function handler(req, res) {
     const url = `${backendUrl}/v1/workspaces/${workspaceId}/clients?archived=false&page-size=1000&sort-column=NAME&sort-order=ASCENDING`;
     
     const response = await fetch(url, {
-      headers: { 'X-Addon-Token': token }, // Koristimo token za komunikaciju sa Clockify-em
-    });
+    headers: { 
+        // PROMENA: Koristite standardni Authorization header umesto X-Addon-Token
+        'Authorization': `Bearer ${token}` 
+    },
+});
 
     if (!response.ok) {
       throw new Error(`Clockify API error: ${response.statusText}`);
